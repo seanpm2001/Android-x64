6819c2c812def8ee1ed819e0b50c970f387ba285
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-21 13:08:02 -0800

    Update cat-make.mk

--- !Src/11.x/11.0/BOOTLOADER/cat-make.mk
+++ !Src/11.x/11.0/BOOTLOADER/cat-make.mk
@@ -1,12 +1,3 @@
 # Makefile
-$(BUILD_DIR)/$(PROJECT)-app.bin: $(BUILD_DIR)/$(PROJECT)-app.elf
-  $(OCPY) $< $@ -O binary
-  $(SZ) $<
-
-$(BUILD_DIR)/$(PROJECT)-boot.bin: $(BUILD_DIR)/$(PROJECT)-boot.elf
-  $(OCPY) --pad-to=0x4000 --gap-fill=0xFF -O binary $< $@
-  $(SZ) $<
-
-# Makefile
 $(BUILD_DIR)/$(PROJECT).bin: $(BUILD_DIR)/$(PROJECT)-boot.bin $(BUILD_DIR)/$(PROJECT)-app.bin
   cat $^ > $@
