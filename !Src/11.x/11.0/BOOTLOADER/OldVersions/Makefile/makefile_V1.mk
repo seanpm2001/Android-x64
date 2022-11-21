af69bc374d8659b80d1459b4f45082d357ef34c0
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-21 13:18:01 -0800

    makefile_V1.mk
    An archive of the first version of the makefile file for this directory set.

---
+++ !Src/11.x/11.0/BOOTLOADER/OldVersions/Makefile/makefile_V1.mk
@@ -0,0 +1,8 @@
+# Makefile
+$(BUILD_DIR)/$(PROJECT)-app.bin: $(BUILD_DIR)/$(PROJECT)-app.elf
+  $(OCPY) $< $@ -O binary
+  $(SZ) $<
+
+$(BUILD_DIR)/$(PROJECT)-boot.bin: $(BUILD_DIR)/$(PROJECT)-boot.elf
+  $(OCPY) --pad-to=0x4000 --gap-fill=0xFF -O binary $< $@
+  $(SZ) $<
